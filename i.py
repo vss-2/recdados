bc41cd2e792fa16310c404d45e945476ff5609d0
vss-2 <vss2@cin.ufpe.br>
2021-03-06 01:36:02 -0300

    Scrapper povoa com base nos 10 exemplos
    i.py e um arquivo vazio usado para facilitar testes usando IDLE no iterativo

---
+++ i.py
@@ -0,0 +1,9 @@
+import requests
+import json
+from random import randint
+from time import sleep
+from getHeaders import getHeaders
+from os import listdir, mkdir
+from os.path import curdir, isdir, exists
+
+print()
--- scrapper2.py
+++ scrapper2.py
@@ -1,7 +1,32 @@
 import requests
+import json
+from random import randint
+from time import sleep
 from getHeaders import getHeaders
-from os import listdir
+from os import listdir, mkdir
+from os.path import curdir, isdir, exists
 from bs4 import BeautifulSoup
+
+def popular_db() -> None:
+    js = None
+    
+    with open('10_exemplos_positivos_e_negativos.json') as arqjson:
+        js = json.load(arqjson)
+
+    for chave in js:
+        if not exists(curdir+'/'+ str(chave.split('.')[0])):
+            mkdir(chave.split('.')[0])
+
+    for chave in js:
+        for valor in js[chave]['positivos']:
+            if not exists(curdir+'/'+str(chave.split('.')[0]+'/'+'{}.html'.format(valor))):
+                with open('{}/{}.html'.format(chave.split('.')[0], valor), 'w') as arqhtml:
+                    output_get = requests.get(url=js[chave]['positivos'][valor], headers=getHeaders())
+                    arqhtml.write(output_get.text)
+                    sleep(randint(15,40))
+            print(chave)
+
+    return None
 
 def main() -> None:
     i = input('Insira o link: ')
@@ -19,4 +44,5 @@ def main() -> None:
     return None
 
 if __name__ == '__main__':
-    main()
+    # main()
+    popular_db()
