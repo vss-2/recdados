cadb2a26dbca0730c41643f743be5bc51433b668
Lucas Vinicius <lvjs@cin.ufpe.br>
2021-03-13 01:03:40 -0300

    feat/fix: crawler adjusts

--- crawler.py
+++ crawler.py
@@ -1,20 +1,49 @@
 from bs4 import BeautifulSoup
 import csv
 from selenium import webdriver
+from time import sleep
+import re
+from json import load
+
 def main():
     cs = None
-    with open('sites.csv', 'r') as arqcsv:
-        linhas = csv.reader(arqcsv, delimiter = ',')
-        # print(next(iter(linhas)))
-        for k in linhas:
-            print(k)
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size: 1920x1080')
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.get("https://twitter.com/search?f=tweets&vertical=default&q=javascript")
-        
+    #options.add_argument('headless')
+    #options.add_argument('window-size: 1920x1080')
+    driver = webdriver.Chrome(options=options)
+    with open("robotsSites.json", "r") as read_file:
+        robotstxt = load(read_file)
+    
+    with open('sites.csv', 'r') as arqcsv:
+        reader = csv.reader(arqcsv, delimiter = ',')
+        rows = list(reader)
+        linkslist = rows[0]
+        visitedlist = []
+        for k in linkslist:
+            count = 0
+            while (count < 1000):
+                count += 1    
+                sitelinks = []
+                sitelinks.append((k,1))   
+                driver.get(sitelinks[count])
+                page = driver.page_source
+                soup = BeautifulSoup(page, 'html.parser')
+                #for link in soup.find_all('a'):
+                #    print(link.get('href'))
     return
+def filter(link) -> bool:
+    disallowCheck()
+    alreadyVisited()
+    
 
+def disallowCheck() -> bool: 
+    x = 0
+def alreadyVisited() -> bool: 
+    x = 0
+def regularExpression(expr, str) -> bool:   
+    result = re.search(expr, str)
+    if result == None:
+        return True
+    return False    
 if __name__ == "__main__":
     main()
