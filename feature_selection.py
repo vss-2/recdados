b201fdb4bb601f7d972d6692e0cb13c6f8a83a50
vss-2 <32624576+vss-2@users.noreply.github.com>
2021-03-25 23:37:10 -0300

    Insercao de funcao auxiliar

--- feature_selection.py
+++ feature_selection.py
@@ -5,6 +5,92 @@ from unicodedata import normalize
 from random import shuffle
 import zipfile
 
+def minerador(sopa, feats):
+    # Implementação do TF-IDF
+    tags_rm = getTagsRem()
+    
+    for t in tags_rm:
+        for element in sopa.findAll(t):
+            element.extract()
+
+    dados = []
+    bingo = [0]*len(feats)
+    score_titulo = 0
+    score_body = 0
+    titulo = ''
+
+    # Tentar pegar informação do título
+    try:
+        if sopa.title.string != None:
+            titulo = sopa.title.string
+            if('.' in titulo):
+                titulo = titulo.replace('.', ' ')
+            if('-' in titulo):
+                titulo = titulo.replace('-', ' ')
+            if(',' in titulo):
+                titulo = titulo.replace(',', ' ')
+            if('|' in titulo):
+                titulo = titulo.replace('|', ' ')
+            if(':' in titulo):
+                titulo = titulo.replace(':', ' ')
+            if('+' in titulo):
+                titulo = titulo.replace('+', ' ')
+            
+            titulo = titulo.casefold().split(' ')
+
+            for p in getPalavrasRem():
+                if p in titulo:
+                    titulo.remove(p)
+
+            titulo = [t.strip() for t in titulo]
+
+            for index, f in enumerate(feats, 0):
+                if f in titulo:
+                    b = titulo.count(f)
+                    if index > len(feats)//2:
+                        score_titulo -= b
+                        bingo[index] = 1
+                    else:
+                        score_titulo += b
+                        bingo[index] = 1
+            
+    except Exception as e:
+        # print('Houve um problema no minerador() durante a avaliacao do score do titulo')
+        pass
+
+    # Tentar pegar informação de qualquer outro campo textual
+    try:
+        tags_sopa = sopa.findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'span', 'p'])
+        for ts in tags_sopa:
+            try:
+                dados.append(ts.getText().casefold().strip())
+            except Exception as e:
+                # print('Houve um problema no minerador() durante a insercao de palavras do body')
+                pass
+    except Exception as e:
+        # print('Houve um problema no minerador() durante a coleta de campos textuais vindo das tags')
+        pass
+
+    try:
+        titulo = list(filter(lambda x: x not in getPalavrasRem(), titulo))
+    except Exception as e:
+        # print('Houve um problema no minerador() durante a remocao de palavras do titulo')
+        pass
+    
+    dados = list(filter(lambda x: x not in getPalavrasRem(), dados))
+
+    for index, f in enumerate(feats, 0):
+        if f in dados:
+            if index > len(feats)//2:
+                score_body -= dados.count(f)
+            else:
+                score_body += dados.count(f)
+
+    bingo[-2] = score_titulo
+    bingo[-1] = score_body
+
+    return bingo
+
 def featureSelection(posneg: str = 'pos', contar: bool = False):
 
     sites = getSitesLista()
@@ -123,4 +209,4 @@ def selecionar(numfeats: int = 9):
                 arqcsv.write(str(x))
                 break
 
-selecionar()
+# selecionar()
