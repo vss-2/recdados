4503cf3fa75a6c1206daf2cc85bc066bb5737fe5
vss-2 <32624576+vss-2@users.noreply.github.com>
2021-04-15 13:13:36 -0300

    Polimento tela de busca

--- front-end/app.py
+++ front-end/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request
+from tarefa4 import extrator
 app = Flask(__name__, static_url_path='', static_folder='', template_folder='')
 
 @app.route("/")
@@ -7,10 +8,11 @@ def home():
 
 @app.route("/teste", methods=['GET'])
 def teste():
-    dados = dict({'Vitor': 21, 'Sousa': 23, 'Silva': 22})
+    dados = extrator()
+    # dados = dict({'Olá': 21})
     busca = request.args.get('busca')
     return render_template("teste.html", len = len(dados), dados = dados)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     
