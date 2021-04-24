ac93a154ea40bee6c4be6bf2086469391d49e37c
Vitor Sousa <32624576+vss-2@users.noreply.github.com>
2021-04-24 00:05:18 -0300

    Implementacao arcaica de Cosseno Score

--- ranking.py
+++ ranking.py
@@ -1,4 +1,6 @@
 from itertools import count
+from sklearn.metrics.pairwise import cosine_similarity
+from numpy import array
 from re import sub
 import pprint
 
@@ -13,8 +15,8 @@ class documento:
     def __init__(self, content = [str]):
         # OBS: caso modificar tratamento, modificar também 
         # na função processar do indiceInvertido
-
-        tratamento = sub('[,.-;:!]', ' ', ' '.join(content).lower())
+        tratamento = ' '.join(content).lower()
+        tratamento = sub('[,.-;:!]', ' ', tratamento)
         self.conteudo = list(filter(lambda x: len(x)>0, tratamento.split(' ')))
         self.score = 0
 
@@ -23,6 +25,9 @@ class documento:
 
     def at_a_time(self, consulta = [str]):
         s = 0
+
+        if (len(consulta) == 1):
+            consulta.append('')
         
         for c in consulta:
             s += self.conteudo.count(c.lower().strip())
@@ -77,6 +82,27 @@ class indiceInvertido:
 
         self.aparicoes = [(v, len(self.vocabulario[v])) for v in self.vocabulario.keys()]
 
+
+def zoneScoring(document, queue = [str], g = dict):
+    # OBS: Mudar a queue conforme campo muda, ex queue[0] é title queue[1] é o body
+
+    # print(queue, (document.at_a_time(consulta = queue) > 0))
+    return g['title'] * (document.at_a_time(consulta = queue) > 0) + g['body'] * (document.at_a_time(consulta = queue) > 0)
+
+def cossenoScore(queue = [str], K = int, index = [indiceInvertido]):
+    scores = []
+    tamanho = [len(i.vocabulario) for i in index]
+    queue = [q.lower().strip() for q in queue]
+    for q in queue:
+        if q in index[0].vocabulario.keys() and q in index[1].vocabulario.keys():
+            scores.append(cosine_similarity(array(index[0].vocabulario[q]), array(index[1].vocabulario[q])))
+        else:
+            print('Palavra procurada:', q ,'não está no índice invertido, vou pular!')
+            continue
+    for d in range(len(scores)):
+        scores[d] = scores[d][0]/tamanho[d]
+    return scores[:K]
+
 def testar():
     # Testando indice invertido
     pp = pprint.PrettyPrinter(indent=4)
@@ -97,7 +123,41 @@ def testar():
         pp.pprint(d.conteudo)
         pp.pprint(d.at_a_time(consulta = ['therefore', 'Let'])) # 3
 
-    # indice()
+    def z_scoring():
+        d = documento(content = [pag1, pag2, pag3, pag4])
+        pesos = dict({'body': 0.6, 'title': 0.4})
+        queue = ['therefore', 'Let']
+        for q in queue:
+            z = zoneScoring(document = d, queue = [q], g = pesos)
+            print('Zone Scoring de ', q, ': ', z, sep='') # 1.0 1.0
+        queue = ['Luciano', 'Let']
+        for q in queue:
+            z = zoneScoring(document = d, queue = [q], g = pesos)
+            print('Zone Scoring de ', q, ': ', z, sep='') # 0.0 1.0
+
+    def c_score():
+        ii = indiceInvertido([pag1, pag2])
+        ii2 = indiceInvertido([pag3, pag4])
+        ii.processar()
+        ii2.processar()
+        # Ele identifica corretamente que to não faz parte de pag3 e pag4
+        # Ele está calculando: Resultado do Cosseno Score: [array([0.11111111, 0.10540926])]
+        print('Resultado do Cosseno Score:',
+            cossenoScore(queue = ['be', 'to'], K = 2, index = [ii, ii2]))
+    
+    indice()
     d_time()
+    z_scoring()
+    c_score()
+    
 
 # testar()
+
+# Funcionamento do Cosseno Score
+# https://datascience.stackexchange.com/questions/26648/cosine-similarity-returns-matrix-instead-of-single-value
+# from sklearn.metrics.pairwise import cosine_similarity
+# from numpy import array
+# vec1 = array([[1,1],[0,1]])
+# vec2 = array([[1,1],[0,1]])
+# #print(cosine_similarity([vec1, vec2]))
+# print(cosine_similarity(vec1, vec2))
