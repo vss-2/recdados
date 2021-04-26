c999972edd6029eb65957c66131a191982d96ecb
godsavemenow <32919964+godsavemenow@users.noreply.github.com>
2021-04-26 02:18:20 -0300

    Update ranking.py

--- ranking.py
+++ ranking.py
@@ -4,7 +4,11 @@ from numpy import array
 from re import sub
 from csv import reader
 from os import listdir
+from numpy import digitize
+from math import log
 import pprint
+import pandas as pd
+import re
 
 pag1 = 'To do is to be to be is to do'
 pag2 = 'To be or not to be I am what I am'
@@ -43,10 +47,11 @@ class documento:
 class indiceInvertido:
 
 
-    def __init__(self, pags):
+    def __init__(self, pags, pols):
         self.vocabulario = dict()
         self.aparicoes = []
         self.paginas = pags
+        self.polegadas = pols
         self.termos = []
         for p in self.paginas:
             # print(p)
@@ -140,7 +145,42 @@ def cosScore(queue = [str], K = int, index = [indiceInvertido]):
 
     scores.sort(key= lambda x: x[0], reverse=True)
     for arq in scores[:K]:
-        print('->>>', index[0].paginas[arq[1]-1], '<<<-')
+        print(arq[0])
+        # print('->>>', index[0].paginas[arq[1]-1], '<<<-')
+
+    return scores[:K]
+
+def cosseno_tfidf(queue = [str], K = int, index = [indiceInvertido]):
+    scores = [0] * len(index[0].termos)
+    length = index[0].termos
+    # print(index[0].vocabulario)
+    
+    for q in queue:
+        # Tratar termo
+        # Calcular importância do termo na query
+        wtq = queue.count(q)/len(queue)
+
+        # Buscar a posting list de cada documento
+        busca = index[0].vocabulario[q]
+        for b in busca:
+            # 'ultrawide': [[218, 1], [226, 1]]
+            scores[b[0]] += b[1] * log(len(index[0].paginas)/b[1], 2) * wtq
+
+        for s in range(len(scores)):
+            scores[s] = scores[s]/length[s]
+
+        # print(scores)
+
+    for s in range(len(scores)):
+        title = index[0].paginas[s].find('.html') + 5
+        title = index[0].paginas[s][title:]
+        scores[s] = (scores[s], s, title.strip())
+        # scores[s] = (scores[s], s, index[0].paginas[s])
+
+    scores.sort(key= lambda x: x[0], reverse=True)
+    for arq in scores[:K]:
+        print(arq[0])
+        # print('<<<-', index[0].paginas[arq[1]-1], '->>>')
 
     return scores[:K]
 
@@ -197,20 +237,81 @@ def testar():
 
 def main():
     pags = []
-    for arq in listdir('./extraído'):
-        with open('./extraído/'+arq, 'r') as arqcsv:
-            linhas = reader(arqcsv)
-            for l in linhas:
+    discretizar = []
+    marca = []
+    tipo_tela = []
+    
+    df = pd.read_csv('./extraído/amazon - amazon.csv')
+
+    # for arq in listdir('./extraído'):
+    with open('./extraído/amazon - amazon.csv', 'r') as arqcsv:
+        linhas = reader(arqcsv)
+        for l in linhas:
+            if l[1].lower().strip() != 'tamanho':
+                posicao_marca = l[0].lower().strip().split(' ')
+                if len(posicao_marca)>1:
+                    l[0] = l[0].strip().lower().split(' ')[1]
+                marca.append(l[0].lower().strip().replace('\'','').replace('\"','').replace(',','.'))
+                discretizar.append(float(l[1].lower().strip().replace('\'','').replace('\"','').replace(',','.').replace(' polegadas','').replace(' centímetros','').replace(' pol','')))
                 pags.append([k.strip()+' ' for k in l])
-            pags.pop(0)
-        # print(pags[1])
+        pags.pop(0)
     
     pags = [''.join(x).lower() for x in pags]
+    # print(df.dtypes)
 
-    ii = indiceInvertido(pags = pags)
+    def tratar_marca(x):
+        t = str(x).strip().lower().split(' ')
+        if len(t)>1:
+            t = t[1]
+            return str(t).lower().strip().replace('\'','').replace('\"','').replace(',','.')
+        return str(x).strip().lower()
+
+    def tratar_tecnologia(x):
+        t = str(x).strip().lower().split(' ')
+        if len(t)>1:
+            if '-' in t: t.remove('-')
+            if 'crystal' in t: t.remove('crystal')
+            if 'ultra' in t: t.remove('ultra')
+            t = t[0]
+            return str(t).lower().strip().replace('\'','').replace('\"','').replace(',','.').replace('\n','').replace('-','')
+        return str(x).strip().lower()
+
+    # def tratar_tela(x):
+    #     re.search(' [0:9][0:9][0:9]')
+
+    df['Marca'] = df['Marca'].map(lambda x: tratar_marca(x))
+    df['Polegadas Discretas'] = digitize(discretizar, [25,50,75,100])
+    df['Tamanho'] = discretizar
+    # df['Tecnologia'] = df['Tecnologia'].map(lambda x: tratar_tecnologia(x))
+
+    # print(df.Tela.unique())
+    # print(pags[1])
+    # print()
+    # print(marca)
+    ii = indiceInvertido(pags = pags, pols = discretizar)
     ii.processar()
 
     pp = pprint.PrettyPrinter(indent=4)
+
+    # print(df.Marca.str.contains('samsung', regex=False))
+
+    def field_index_idf(termo, campo):
+        idf = []
+        for x in range(len(df[campo])):
+            times = df[campo][x].count(termo)
+            if times > 0:
+                idf.append((x, times))
+        return idf
+
+    # print(field_index_idf('lg', 'Marca'))
+
+    # field_index = pd.DataFrame()
+    # df['Marca']
+    # df['Tamanho']
+    # df['Tecnologia']
+    # df['Tela']
+    # df['Entrada']
+
     # pp.pprint(pags[1])
     # pp.pprint(sorted(ii.vocabulario))
     # pp.pprint(ii.vocabulario)
@@ -231,6 +332,7 @@ def main():
                 print('Zone Scoring de ', b, ': ', z, sep='')
 
     cs = cosScore(queue = ['tcl'], K = 10, index = [ii])
+    cs = cosseno_tfidf(queue = ['tcl'], K = 10, index = [ii])
     # pp.pprint(cs)
 
 main()
